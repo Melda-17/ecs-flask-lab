e0b4d4d3f311512dd4eef7fb2db7069365222203
Melda-17 <meldasabu17@gmail.com>
2025-04-08 21:35:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,5 @@ def home():
     return "Hello from melda ECS Container"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Use 8080 from Cloud Run
+    app.run(host="0.0.0.0", port=port)
